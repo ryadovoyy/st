1e3f0f976a21c41334f46bfdaea20d165916b436
ryadovoyy <holstinindvs@gmail.com>
2022-07-21 08:02:57 +0700

    Change font to SpaceMono

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Roboto Mono:pixelsize=15:antialias=true";
+static char *font = "SpaceMono Nerd Font:pixelsize=15:antialias=true:autohint=true";
 static int borderpx = 7;
 
 /*
