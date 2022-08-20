d12732a9ea886fe0e4319c4f21c0ca9dc8a3badf
ryadovoyy <holstinindvs@gmail.com>
2022-08-20 15:38:52 +0700

    Change font to JetBrainsMonoNL

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "SpaceMono Nerd Font:pixelsize=15:antialias=true:autohint=true";
+static char *font = "JetBrainsMonoNL Nerd Font:pixelsize=15:antialias=true:autohint=true";
 static int borderpx = 7;
 
 /*
