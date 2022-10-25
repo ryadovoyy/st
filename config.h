8792bb0fe225a4a0d20f5972fe661c79d55cc501
ryadovoyy <holstinindvs@gmail.com>
2022-10-26 05:34:49 +0700

    Increase borderpx

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "JetBrainsMonoNL Nerd Font:pixelsize=15:antialias=true:autohint=true";
-static int borderpx = 7;
+static int borderpx = 8;
 
 /*
  * What program is execed by st depends of these precedence rules:
